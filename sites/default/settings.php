d18d72d937dfb37dc5fc1048def691f79e58227d
Rob McKenzie <mckenro@gmail.com>
2017-06-07 10:31:58 -0700

    add sub theme and attempt to make own theme from scratch

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -245,15 +245,15 @@
  * @endcode
  */
 $databases = array (
-  'default' => 
+  'default' =>
   array (
-    'default' => 
+    'default' =>
     array (
       'database' => 'recipes',
       'username' => 'recipe',
       'password' => 'recipe',
-      'host' => 'localhost',
-      'port' => '',
+      'host' => '127.0.0.1',
+      'port' => '8889',
       'driver' => 'mysql',
       'prefix' => '',
     ),
